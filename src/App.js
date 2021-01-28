4bafcdb5ec8ca44cbfa5b2105bfd70c3453b0115
Justin Bartlett <finnquib@googlemail.com>
2021-01-28 17:04:38 +0000

    Added 'about, contact, footer, header, home, navbar' components. Updated app to use Routes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Contact from './components/Contact'
 import './App.css';
 
 function App() {
   return (
     <main>
-      <h1>Hello</h1>
+    <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </main>
-  );
+  )
 }
 
 export default App;
