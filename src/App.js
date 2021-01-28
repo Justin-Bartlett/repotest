9c9aa1903bbc528cf157215e8d1d9621caa7f160
Justin Bartlett <finnquib@googlemail.com>
2021-01-28 17:06:02 +0000

    added forgotten 'about' component import to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import About from './components/About'
 import Contact from './components/Contact'
 import './App.css';
 
