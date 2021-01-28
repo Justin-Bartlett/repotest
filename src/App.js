4fc7cd27f1a7bb037fef202517f3dc7727d67a99
Justin Bartlett <finnquib@googlemail.com>
2021-01-28 17:10:10 +0000

    added missing imports

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import {Route, Switch} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Contact from './components/Contact'
 import './App.css';
 
 function App() {
   return (
     <main>
-    <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <Header />
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      <Footer />
     </main>
   )
 }
